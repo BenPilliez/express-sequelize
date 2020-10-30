2d3c662d29a7d19d8f6a9db46db1aa5747342fb3
Benjamin Pilliez <benjamin.pilliez@gmail.com>
2020-10-30 12:19:52 +0100

    fix dev mod

--- db/config.js
+++ db/config.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 
 module.exports = {
     "development": {
-        "username": process.env.DB_USER,
-        "password": process.env.DB_PASSWORD,
-        "database": process.env.DB_NAME,
-        "host": process.env.CLEARDB_DATABASE_URL,
+        "username": process.env.DB_DEV_USER,
+        "password": process.env.DB_DEV_PASSWORD,
+        "database": process.env.DB_DEV_NAME,
+        "host": '0.0.0.0',
         "dialect": "mysql"
     },
     "test": {
